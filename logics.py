29b77679a9b8712ec9ee07f1e65524f3903bb61e
aliyancat <aliyanmunawwaralis@gmail.com>
2026-06-26 05:54:34 -0700

    Auto-commit: 1 modified file

---
+++ logics.py
@@ -0,0 +1,18 @@
+
+
+#To store something
+#   We use  variables   when  there  is only   one thing  to store
+
+health =   90
+name =  "Ali"
+
+# When do we  use   an Array / List ?
+#  -->  When we  have mutlply  values
+
+#Example :  Name of your class fellows
+class_fellows = ["Ali", "Ahmed", "Ayesha"]
+
+
+
+#When to   use a  dictionary
+# ->  When an object has some information about   it 
