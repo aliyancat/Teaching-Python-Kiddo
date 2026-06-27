b7281e80e994974fb5dcc23e0eda43756de31eb7
aliyancat <aliyanmunawwaralis@gmail.com>
2026-06-27 05:40:31 -0700

    Auto-commit: 1 modified file

--- logics.py
+++ logics.py
@@ -15,4 +15,34 @@ class_fellows = ["Ali", "Ahmed", "Ayesha"]
 
 
 #When to   use a  dictionary
-# ->  When an object has some information about   it 
+# ->  When an object has some information about  it 
+
+Movie = {
+"Name":  "Kung Fu Panda",
+"Year":  2010,
+"Rating":  8.5,
+"Characters": ["Po","Shifu","Tigress"]
+}
+
+
+#Whenever we   need to make  a   decision
+#   we  use  if condition statement
+
+marks = 80
+
+if  marks >= 90:
+    print("A+") 
+elif marks >= 80:
+    print("A")      
+elif marks >= 70:
+    print("B")      
+elif marks >= 60:
+    print("C")  
+else:
+    print("Fail")
+
+# When we  need  to  repeat  an  action
+#  ->  we  use  LOOPS
+#  -> TYPES OF  LOOPS:
+#   1. For Loop -> When we  already know how  many times  it  will repeat
+#   2. While Loop -> Runs until a condition is met
